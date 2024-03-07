e6f12ddccfc09634134fb913edb1c02acf79c5b7
joc-rgb <bestoneisme123@gmail.com>
2024-03-07 14:08:19 +0800

    Place Clerk readymade UserButton in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
     return (
-      <div className="p-1">
-        <Button  >Click Me</Button>
+      <div className="p-4">
+        <UserButton afterSignOutUrl="/" />
       </div>
     );
   }
