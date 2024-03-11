868ef53f491702a1c50aa8b9d1e68f3d8b9bba0a
joc-rgb <bestoneisme123@gmail.com>
2024-03-11 16:30:03 +0800

    Continue onSubmit function for store modal

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as z from 'zod'
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from '../ui/modal'
 import { useStoreModal } from '@/hooks/use-store-modal'
 import { useForm } from 'react-hook-form'
@@ -9,6 +9,9 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import { ReloadIcon } from '@radix-ui/react-icons'
+import axios from "axios";
+import toast from 'react-hot-toast'
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -19,6 +22,8 @@ const formSchema = z.object({
 export const StoreModal = () => {
 
   const storeModal = useStoreModal();
+  const [isLoading, setLoading] = useState(false)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues:{
@@ -27,7 +32,17 @@ export const StoreModal = () => {
   })
 
   const onSubmit = async (values: z.infer<typeof formSchema>)=>{
-    console.log(values)
+    try {
+      setLoading(true);
+      const res = await axios.post('/api/stores', values)
+      toast.success("Store created successfully.")
+      console.log(res.data)
+    } catch (error) {
+      toast.error("Something went wrong.");
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
   
   return (
@@ -47,7 +62,7 @@ export const StoreModal = () => {
                 <FormItem>
                   <FormLabel>Store Name</FormLabel>
                   <FormControl>
-                    <Input placeholder='Your Store Name. eg: Janice&apos;s Cosmetic' {...field} />
+                    <Input disabled={isLoading} placeholder='Your Store Name. eg: Janice&apos;s Cosmetic' {...field} />
                   </FormControl>
                   <FormDescription>
                     This is your store name. No worries, it can be changed anytime.
@@ -57,8 +72,12 @@ export const StoreModal = () => {
               )}
             />
             <div className="pt-4 justify-end items-center flex space-x-2 w-full">
-              <Button type='button' variant={'outline'} onClick={storeModal.onClose} >Cancel</Button>
-              <Button type='submit'>Continue</Button>
+              <Button disabled={isLoading} type='button' variant={'outline'} onClick={storeModal.onClose} >Cancel</Button>
+              {!isLoading ? <Button disabled={isLoading} type='submit'>Continue</Button>
+              :<Button disabled>
+                <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>}
             </div>
             
           </form>
