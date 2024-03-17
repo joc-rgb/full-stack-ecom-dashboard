691239788cfef5095b9919f4a01ebfb40ef822ec
joc-rgb <bestoneisme123@gmail.com>
2024-03-17 21:44:55 +0800

    Debugging import error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,25 @@
-"use client"
-
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, auth } from '@clerk/nextjs'
 import React from 'react'
 import MainNav from './main-nav'
+import StoreSwitcher from './store-switcher'
+import prismadb from '@/lib/prismadb'
+import { redirect } from 'next/navigation'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const {userId} = auth()
+  if(!userId){
+    redirect('/sign-in')
+  }
+  const stores = await prismadb.store.findMany({
+    where:
+    {
+      userId
+    }
+  })
   return (
     <div className='flex'>
       <div className="flex-col h-screen border-r p-2 space-y-10">
-        <div>
-            This is a store switcher
-        </div>
+        <StoreSwitcher items={stores}/>
         <MainNav className='space-y-6 w-full'/>
       </div>
       <div className="flex px-2 h-16 w-full items-center border-b justify-around">
