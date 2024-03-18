5fe83339ae00460bc10e2ee98c030bd996b51f9b
joc-rgb <bestoneisme123@gmail.com>
2024-03-18 17:19:21 +0800

    Create reusable Heading components

---
+++ components/ui/heading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+interface HeadingProps {
+    title: string,
+    description :string|null
+}
+const Heading: React.FC<HeadingProps> = ({title, description}) => {
+  return (
+    <div>
+      <h2 className='text-2xl font-bold'>{title}</h2>
+      <p className='text-sm text-muted-foreground'>{description}</p>
+    </div>
+  )
+}
+
+export default Heading
