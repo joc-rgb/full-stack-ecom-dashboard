4b6d782678d62d630ff132928be24dcdae82e8eb
joc-rgb <bestoneisme123@gmail.com>
2024-03-23 16:10:11 +0800

    Add Billboards component

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Billboard from './components/billboard'
+
+const Billboards = () => {
+  return (
+    <div>
+      <Billboard />
+    </div>
+  )
+}
+
+export default Billboards
